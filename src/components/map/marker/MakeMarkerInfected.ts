692083bdb5c511a7a22ce90dc1f18d83337fcaca
1seok2 <misohaha31@naver.com>
2020-08-06 05:00:42 +0900

    add marker click event

--- src/components/map/marker/MakeMarkerInfected.ts
+++ src/components/map/marker/MakeMarkerInfected.ts
@@ -12,24 +12,21 @@ const MakeMarkerInfected = (map : any,_patient : any, color:string) => {
     marker.setMap(map);
 
     // 마커에 커서가 오버됐을 때 마커 위에 표시할 인포윈도우를 생성합니다
-    var iwContent = `<div style="padding:5px;">${_patient.position}</div>`; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    var iwContent = `<div style="padding:5px;">${_patient.position}</div>`,      // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+            iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다;
 
     // 인포윈도우를 생성합니다
     var infowindow = new window.kakao.maps.InfoWindow({
-        content : iwContent
+        content : iwContent,
+        removable : iwRemoveable
     });
 
-    // 마커에 마우스오버 이벤트를 등록합니다
-    window.kakao.maps.event.addListener(marker, 'mouseover', function() {
-      // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
-        infowindow.open(map, marker);
+    // 마커에 클릭이벤트를 등록합니다
+    window.kakao.maps.event.addListener(marker, 'click', function() {
+        // 마커 위에 인포윈도우를 표시합니다
+        infowindow.open(map, marker);  
     });
 
-    // 마커에 마우스아웃 이벤트를 등록합니다
-    window.kakao.maps.event.addListener(marker, 'mouseout', function() {
-        // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
-        infowindow.close();
-    });
 
     const circle = new window.kakao.maps.Circle({
       center: new window.kakao.maps.LatLng(_patient.lat, _patient.lng), // 원의 중심좌표 입니다
