70269fb0d10dd8bcae3481454658e9ec1e12a533
1seok2 <misohaha31@naver.com>
2020-08-13 15:42:56 +0900

    add last updated time

--- src/components/map/compose/MapNav.tsx
+++ src/components/map/compose/MapNav.tsx
@@ -15,6 +15,7 @@ const MapNav = () => {
                 <li className="navRed">
                     <i className="icon-circle icon-circle-red"></i> 3일 이내
                 </li>
+                <li style={{textAlign:"right", fontSize:".7rem"}}><span style={{fontSize:'.5rem'}}>마지막 업데이트 </span>'200813 13:00</li>
             </ul>
         </>
     );
