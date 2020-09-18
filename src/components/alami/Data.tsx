3c02a335cfe62dd7e060ad862cac458a9a7ded56
1seok2 <misohaha31@naver.com>
2020-09-18 11:08:40 +0900

    update notice

--- src/components/alami/Data.tsx
+++ src/components/alami/Data.tsx
@@ -1,76 +1,94 @@
-import React, { Component, useState, useEffect } from 'react';
-import MakeSentence from './MakeSentence';
-import OtherMenu from '../navigation/OtherMenu';
-import AlertModal from './modal/alertModal';
-import Tips from './Tips'
-import '../../style/Data.css';
-import '../../style/fontello-6de7bc38/css/mapticon-embedded.css';
+import React, { Component, useState, useEffect } from "react";
+import MakeSentence from "./MakeSentence";
+import OtherMenu from "../navigation/OtherMenu";
+import AlertModal from "./modal/alertModal";
+import Tips from "./Tips";
+import "../../style/Data.css";
+import "../../style/fontello-6de7bc38/css/mapticon-embedded.css";
 
 interface DataProps {
-  lat : number,
-  lng : number,
-  patientNum : number,
-  alami : any
+  lat: number;
+  lng: number;
+  patientNum: number;
+  alami: any;
 }
 
-const Data = (props : DataProps) => {
-  const [btnState, setBtnState] = useState('none');
-  const [btnValue, setBtnValue] = useState('icon-emo-wink');
+const Data = (props: DataProps) => {
+  const [btnState, setBtnState] = useState("none");
+  const [btnValue, setBtnValue] = useState("icon-emo-wink");
   const [nowTime, setNowTime] = useState({
-    nowYear : '',
-    nowMonth : '',
-    nowDate : '',
-    nowHour : '',
-    nowMin : ''
+    nowYear: "",
+    nowMonth: "",
+    nowDate: "",
+    nowHour: "",
+    nowMin: "",
   });
 
   const onClickPos = () => {
     //init();
-    const data : any= document.getElementById('dataContainer');
-    if(btnState === 'show'){
-      data.classList.remove('show');
-      data.classList.add('none');
-      setBtnState('none');
-      setBtnValue('icon-emo-wink');
+    const data: any = document.getElementById("dataContainer");
+    if (btnState === "show") {
+      data.classList.remove("show");
+      data.classList.add("none");
+      setBtnState("none");
+      setBtnValue("icon-emo-wink");
     } else {
-      data.classList.remove('none');
-      data.classList.add('show');
-      setBtnState('show');
-      setBtnValue('icon-map-o');
+      data.classList.remove("none");
+      data.classList.add("show");
+      setBtnState("show");
+      setBtnValue("icon-map-o");
     }
   };
 
   const getNowTime = () => {
     let nowTime = new Date();
     setNowTime({
-      nowYear : `${nowTime.getFullYear()}`,
-      nowMonth : nowTime.getMonth()+1 < 10 ? (`0${nowTime.getMonth()+1}`) : `${nowTime.getMonth()+1}`,
-      nowDate : nowTime.getDate() < 10 ? (`0${nowTime.getDate()}`) : `${nowTime.getDate()}`,
-      nowHour : nowTime.getHours() < 10 ? (`0${nowTime.getHours()}`) : `${nowTime.getHours()}`,
-      nowMin : nowTime.getMinutes() < 10 ? (`0${nowTime.getMinutes()}`) : `${nowTime.getMinutes()}`
+      nowYear: `${nowTime.getFullYear()}`,
+      nowMonth:
+        nowTime.getMonth() + 1 < 10
+          ? `0${nowTime.getMonth() + 1}`
+          : `${nowTime.getMonth() + 1}`,
+      nowDate:
+        nowTime.getDate() < 10
+          ? `0${nowTime.getDate()}`
+          : `${nowTime.getDate()}`,
+      nowHour:
+        nowTime.getHours() < 10
+          ? `0${nowTime.getHours()}`
+          : `${nowTime.getHours()}`,
+      nowMin:
+        nowTime.getMinutes() < 10
+          ? `0${nowTime.getMinutes()}`
+          : `${nowTime.getMinutes()}`,
     });
-  }
+  };
 
   useEffect(() => {
     getNowTime();
-    const Container : any = document.getElementById('dataContainer');
+    const Container: any = document.getElementById("dataContainer");
     Container.style.backgroundColor = props.alami.conditionBgColor;
-    console.log('rerender');
-  },[nowTime.nowMin]);
-
-  
+    console.log("rerender");
+  }, [nowTime.nowMin]);
 
   return (
     <>
       <Tips />
-      <AlertModal idNum={0} contents={[
-        "⚠️ 필독 ⚠️",
-        "위치 기본 값은 수서역 입니다",
-        "우측 하단의 현위치 버튼으로 위치 재설정 가능합니다",
-        "확진자가 방문한 곳을 표시합니다",
-        "돋보기 버튼으로 다른지역을 검색하여 볼 수 있습니다"
-      ]}/>
-      <div className="dataContainer none" id="dataContainer" style={{backgroundColor:`${props.alami.conditionBgColor}`}}>
+      <AlertModal
+        idNum={0}
+        contents={[
+          "⚠️ 필독 ⚠️",
+          // "위치 기본 값은 수서역 입니다",
+          // "우측 하단의 현위치 버튼으로 위치 재설정 가능합니다",
+          // "확진자가 방문한 곳을 표시합니다",
+          // "돋보기 버튼으로 다른지역을 검색하여 볼 수 있습니다"
+          "현재 코로나맵 리뉴얼 작업중에 있어 당분간 확진자 업데이트가 최신화되지 않을 수 있습니다. 빠른 시일내에 돌아오도록 하겠습니다.",
+        ]}
+      />
+      <div
+        className="dataContainer none"
+        id="dataContainer"
+        style={{ backgroundColor: `${props.alami.conditionBgColor}` }}
+      >
         <div className="other-menu-wrapper">
           <OtherMenu />
         </div>
@@ -78,7 +96,10 @@ const Data = (props : DataProps) => {
           <div className="header">
             {/* 현 주소와 시간 표시 */}
             <h1 id="centerAddr">위치확인이 필요합니다</h1>
-            <h2 id="now-time">{nowTime.nowYear}-{nowTime.nowMonth}-{nowTime.nowDate} {nowTime.nowHour}:{nowTime.nowMin}</h2>
+            <h2 id="now-time">
+              {nowTime.nowYear}-{nowTime.nowMonth}-{nowTime.nowDate}{" "}
+              {nowTime.nowHour}:{nowTime.nowMin}
+            </h2>
           </div>
           <div className="contents">
             <div id="conditionFace">
@@ -87,7 +108,9 @@ const Data = (props : DataProps) => {
             <div className="data-list">
               <h3 id="conditionTxt">{props.alami.conditionTxt}</h3>
               <MakeSentence state={props.alami.conditionState} />
-              <p className="data-list-part">주변 코로나환자수 : {props.patientNum} 명</p>
+              <p className="data-list-part">
+                주변 코로나환자수 : {props.patientNum} 명
+              </p>
             </div>
           </div>
         </div>
@@ -97,6 +120,5 @@ const Data = (props : DataProps) => {
       </a>
     </>
   );
-
-}
+};
 export default Data;
