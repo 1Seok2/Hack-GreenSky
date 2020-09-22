110a156e167768684c2d465ccca8acf126336796
1seok2 <misohaha31@naver.com>
2020-09-22 10:00:08 +0900

    update json 200922

--- src/components/map/compose/MapNav.tsx
+++ src/components/map/compose/MapNav.tsx
@@ -1,29 +1,27 @@
-import React from 'react';
-import '../../../style/fontello-6de7bc38/css/mapticon-embedded.css';
+import React from "react";
+import "../../../style/fontello-6de7bc38/css/mapticon-embedded.css";
 
 const MapNav = () => {
-    return (
-        <>
-            <ul className="mapNav">
-                {/* <li className="mapNav-list-title">확진자 발생 추이</li> */}
-                <li className="navGrn">
-                    <i className="icon-circle icon-circle-green"></i> 8~14 일 사이
-                </li>
-                <li className="navOrg">
-                    <i className="icon-circle icon-circle-orange"></i> 4~7 일 사이
-                </li>
-                <li className="navRed">
-                    <i className="icon-circle icon-circle-red"></i> 3일 이내
-                </li>
-                <li style={{textAlign:"right", fontSize:".7rem"}}>
-                    <span style={{fontSize:'.5rem'}}>
-                        마지막 업데이트
-                    </span>
-                    '2009016 10:00
-                </li>
-            </ul>
-        </>
-    );
-}
+  return (
+    <>
+      <ul className="mapNav">
+        {/* <li className="mapNav-list-title">확진자 발생 추이</li> */}
+        <li className="navGrn">
+          <i className="icon-circle icon-circle-green"></i> 8~14 일 사이
+        </li>
+        <li className="navOrg">
+          <i className="icon-circle icon-circle-orange"></i> 4~7 일 사이
+        </li>
+        <li className="navRed">
+          <i className="icon-circle icon-circle-red"></i> 3일 이내
+        </li>
+        <li style={{ textAlign: "right", fontSize: ".7rem" }}>
+          <span style={{ fontSize: ".5rem" }}>마지막 업데이트</span>
+          '2009022 10:00
+        </li>
+      </ul>
+    </>
+  );
+};
 
 export default MapNav;
