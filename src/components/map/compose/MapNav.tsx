ddf42da17e544f671dc0c6e314638cf8d7f6d2ac
1seok2 <misohaha31@naver.com>
2020-10-24 13:14:24 +0900

    update json 201024

--- src/components/map/compose/MapNav.tsx
+++ src/components/map/compose/MapNav.tsx
@@ -7,24 +7,17 @@ const MapNav = () => {
       <ul className="mapNav">
         {/* <li className="mapNav-list-title">확진자 발생 추이</li> */}
         <li className="navGrn">
-          <i className="icon-circle icon-circle-green"></i>{" "}
-          8~14 일 사이
+          <i className="icon-circle icon-circle-green"></i> 8~14 일 사이
         </li>
         <li className="navOrg">
-          <i className="icon-circle icon-circle-orange"></i>{" "}
-          4~7 일 사이
+          <i className="icon-circle icon-circle-orange"></i> 4~7 일 사이
         </li>
         <li className="navRed">
-          <i className="icon-circle icon-circle-red"></i>{" "}
-          3일 이내
+          <i className="icon-circle icon-circle-red"></i> 3일 이내
         </li>
-        <li
-          style={{ textAlign: "right", fontSize: ".7rem" }}
-        >
-          <span style={{ fontSize: ".5rem" }}>
-            마지막 업데이트
-          </span>
-          '201021 10:03
+        <li style={{ textAlign: "right", fontSize: ".7rem" }}>
+          <span style={{ fontSize: ".5rem" }}>마지막 업데이트</span>
+          '201024 13:14
         </li>
       </ul>
     </>
