c399a5a4f3192bd5110d917cc81e0d563dd7bbf7
1seok2 <misohaha31@naver.com>
2020-11-28 21:45:03 +0900

    update json 201128

--- src/components/map/compose/MapNav.tsx
+++ src/components/map/compose/MapNav.tsx
@@ -17,7 +17,7 @@ const MapNav = () => {
         </li>
         <li style={{ textAlign: "right", fontSize: ".7rem" }}>
           <span style={{ fontSize: ".5rem" }}>마지막 업데이트</span>
-          '201127 17:42
+          '201128 21:44
         </li>
       </ul>
     </>
